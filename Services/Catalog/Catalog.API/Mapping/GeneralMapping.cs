a462c248f66032c2ce2b1b27e26e7923e9457914
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-02-14 22:54:09 +0300

    .

---
+++ Services/Catalog/Catalog.API/Mapping/GeneralMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Catalog.API.Dtos;
+using Catalog.API.Models;
+using MongoDB.Driver.Core.Operations;
+
+namespace Catalog.API.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        }
+    }
+}
